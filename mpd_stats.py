eae21eee8841497f330f2c70d060de76e80d518c
Hiroto Kaku <20251225+thekakkun@users.noreply.github.com>
2023-09-04 11:55:32 -0400

    Define required and optional keys for Status and Track

--- mpd_stats.py
+++ mpd_stats.py
@@ -5,62 +5,66 @@ from typing import Literal, Optional, TypedDict
 
 from mpd.asyncio import MPDClient
 
-Status = TypedDict(
-    "Status",
-    {
-        "repeat": Literal["0", "1"],
-        "random": Literal["0", "1"],
-        "single": Literal["0", "1", "oneshot"],
-        "consume": Literal["0", "1", "oneshot"],
-        "partition": str,
-        "playlist": str,
-        "playlistlength": str,
-        "mixrampdb": str,
-        "state": Literal["play", "stop", "pause"],
-        "song": str,
-        "songid": str,
-        "time": str,
-        "elapsed": str,
-        "bitrate": str,
-        "duration": str,
-        "audio": str,
-        "nextsong": str,
-        "nextsongid": str,
-    },
-    total=False,
-)
 
-Track = TypedDict(
-    "Track",
+class StatusBase(TypedDict):
+    repeat: Literal["0", "1"]
+    random: Literal["0", "1"]
+    single: Literal["0", "1", "oneshot"]
+    consume: Literal["0", "1", "oneshot"]
+    partition: str
+    playlist: str
+    playlistlength: str
+    mixrampdb: str
+    state: Literal["play", "stop", "pause"]
+
+
+class Status(StatusBase, total=False):
+    song: str
+    songid: str
+    time: str
+    elapsed: str
+    bitrate: str
+    duration: str
+    audio: str
+    nextsong: str
+    nextsongid: str
+
+
+# Using functional syntax because "last-modified" has a dash
+TrackBase = TypedDict(
+    "TrackBase",
     {
         "file": str,
         "last-modified": str,
         "format": str,
-        "artist": str,
-        "albumartist": str,
-        "artistsort": str,
-        "title": str,
-        "album": str,
-        "track": str,
-        "date": str,
-        "originaldate": str,
-        "genre": str,
-        "disc": str,
-        "label": str,
-        "albumartistsort": str,
-        "musicbrainz_workid": str,
-        "musicbrainz_albumid": str,
-        "musicbrainz_artistid": str,
-        "musicbrainz_albumartistid": str,
-        "musicbrainz_releasetrackid": str,
-        "musicbrainz_trackid": str,
-        "time": str,
         "duration": str,
+        "time": str,
         "pos": str,
         "id": str,
     },
-    total=False,
 )
+
+
+class Track(TrackBase, total=False):
+    artist: str
+    albumartist: str
+    artistsort: str
+    title: str
+    album: str
+    track: str
+    date: str
+    originaldate: str
+    genre: str
+    disc: str
+    label: str
+    albumartistsort: str
+    musicbrainz_workid: str
+    musicbrainz_albumid: str
+    musicbrainz_artistid: str
+    musicbrainz_albumartistid: str
+    musicbrainz_releasetrackid: str
+    musicbrainz_trackid: str
+
 
 Subsystems = Literal[
     "database",
